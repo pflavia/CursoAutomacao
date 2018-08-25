ab957732d8b4602a61f6d43ba3d5438331efb733
Flávia Pinheiro <pflavia@users.noreply.github.com>
2018-08-24 22:17:57 -0300

    Com print

--- webelementstest/src/test/java/webelementstest/Arrastar.java
+++ webelementstest/src/test/java/webelementstest/Arrastar.java
@@ -8,10 +8,16 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+
+import org.openqa.selenium.TakesScreenshot;
+import org.apache.commons.io.FileUtils;
+import java.io.File;
+import java.io.IOException;
 
 public class Arrastar {
 
@@ -33,14 +39,28 @@ public class Arrastar {
 
 	@Test
 	public void test() throws InterruptedException {
-				
+
 		WebElement Origem = driver.findElement(By.id("draggable"));
-		
+
 		WebElement Destino = driver.findElement(By.id("droppable"));
 		new Actions(driver).dragAndDrop(Origem, Destino).perform();
-		
+
 		assertEquals("Dropped!",Destino.getText());
-		
+
 	}
+	@Test
+	public void print() throws InterruptedException, IOException {
+
+		WebElement Origem = driver.findElement(By.id("draggable"));
+
+		WebElement Destino = driver.findElement(By.id("droppable"));
+		new Actions(driver).dragAndDrop(Origem, Destino).perform();
+
+		File scrnShot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(scrnShot , new File("c:\\Users\\aluno\\flavia-workspace\\main_page.png"));
+
+
+	}
+
 
 }
